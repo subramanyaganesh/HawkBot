82877ebc690252cf2cbd5ad7f82303270e8a9e7e
darshannair11 <127619097+darshannair11@users.noreply.github.com>
2023-12-23 22:43:46 +0400

    Delete src directory

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import App from './Components/App';
-import {createRoot} from "react-dom/client"
-import "./styles.css";
-
-const root=createRoot(document.getElementById('root'))
-root.render(<App />);
