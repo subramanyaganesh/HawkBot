fa3d7f055f8f7586ad00e0d36a6b607234ccf9ed
subramanyaganesh <subramanya1174@gmail.com>
2024-06-12 17:35:34 -0500

    adding a flag to remove comments on UI

--- server/iitbot/serializers.py
+++ server/iitbot/serializers.py
@@ -3,4 +3,5 @@ from rest_framework import serializers
 class JsonResponseSerializer(serializers.Serializer):
     answer = serializers.CharField()
     sources = serializers.ListField(child=serializers.CharField())
+    flag = serializers.BooleanField()
 
